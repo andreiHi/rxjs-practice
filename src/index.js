eab7f4444615dec7772d7577354f908e54fc0e20
Andrei Hincu <a.hincu@octopod.ru>
2020-01-05 23:24:54 +0200

    Создание своего Redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-import {EMPTY, fromEvent} from 'rxjs'
-import {map, debounceTime, distinctUntilChanged, switchMap, mergeMap, tap, catchError, filter} from 'rxjs/operators'
+import {EMPTY, fromEvent, Subject} from 'rxjs'
+import {
+    map,
+    debounceTime,
+    distinctUntilChanged,
+    switchMap,
+    mergeMap,
+    tap,
+    catchError,
+    filter,
+    scan, startWith, shareReplay
+} from 'rxjs/operators'
 import {ajax} from 'rxjs/ajax'
 
 const url = 'https://api.github.com/search/users?q=';
@@ -36,3 +46,54 @@ stream$.subscribe(
       </div>`;
         result.insertAdjacentHTML('beforeend', html);
     });
+
+
+
+const pre = document.querySelector('pre');
+const handlers = {
+    INCREMENT: state => ({...state, counter: state.counter + 1}),
+    DECREMENT: state => ({...state, counter: state.counter - 1}),
+    ADD: (state, action) => ({...state, counter: state.counter + action.payload}),
+    DEFAULT: state => state
+};
+const  initialState = {
+    counter: 0
+};
+
+function reducer(state = initialState, action) {
+        // switch (action.type) {
+        //     case 'INCREMENT' : return {...state, counter: state.counter +  1};
+        //     case 'DECREMENT' : return {...state, counter: state.counter -  1};
+        //     case 'ADD' : return {...state, counter: state.counter +  action.payload};
+        //     default: return state
+        // }
+    const handler = handlers[action.type] || handlers.DEFAULT;
+    return handler(state, action);
+}
+
+function createStore(rootReducer) {
+    const sabj$ = new Subject();
+    const store$ = sabj$.pipe(
+        startWith({type: '_INIT_'}),
+        scan(rootReducer, undefined),  // rootReducer то что должно быть выполнено , undefined начальное значение
+        shareReplay(1)
+    );
+    store$.dispatch = action => sabj$.next(action);
+    return store$;
+}
+
+const store$ = createStore(reducer);
+
+store$.subscribe(state =>{
+pre.innerHTML = JSON.stringify(state, null, 2)
+});
+
+document.getElementById('increment').addEventListener('click', () =>{
+    store$.dispatch({type: 'INCREMENT'})
+});
+document.getElementById('decrement').addEventListener('click', () =>{
+    store$.dispatch({type: 'DECREMENT'})
+});
+document.getElementById('add').addEventListener('click', () =>{
+    store$.dispatch({type: 'ADD', payload: 10})
+});
